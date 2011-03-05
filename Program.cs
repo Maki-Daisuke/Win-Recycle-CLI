be9e724534a51033dfcdf94c66c0251b795f52d1
Daisuke (yet another) Maki <maki.daisuke@gmail.com>
2011-03-06 03:44:46 +0900

    Initial comit.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Recycle
+{
+    public class Recycle
+    {
+        public static void Main(string[] args) {
+            try {
+                foreach (var i in args) {
+                    FileSystem.DeleteFile(i, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin, UICancelOption.ThrowException);
+                }
+            } catch ( Exception e ) {
+                System.Console.Error.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
